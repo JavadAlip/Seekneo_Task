3d591d94bb1c1cea83a5a5df6be3ccbd39354a61
JavadAlip <javadalipp911@gmail.com>
2025-07-19 13:39:49 +0530

    Created a separate data file

---
+++ data.js
@@ -0,0 +1,8 @@
+
+const dummyData = [
+  { name: "John Doe", email: "john@example.com", age: 28, place: "Kerala" },
+  { name: "Jane Smith", email: "jane@example.com", age: 34, place: "Mumbai"  },
+  { name: "Mike Johnson", email: "mike@example.com", age: 22, place: "Kerala"  },
+];
+
+module.exports = dummyData;
